161181c88a9ba2706fb078f53ee4e02146c2f1a8
Minecrell <minecrell@minecrell.net>
2020-04-01 22:41:25 +0200

    some work on voice call audio

--- sound/soc/qcom/qdsp6/q6routing.c
+++ sound/soc/qcom/qdsp6/q6routing.c
@@ -653,6 +653,9 @@ static const struct snd_soc_dapm_widget msm_qdsp6_widgets[] = {
 	SND_SOC_DAPM_AIF_OUT("MM_UL7", "MultiMedia7 Capture", 0, 0, 0, 0),
 	SND_SOC_DAPM_AIF_OUT("MM_UL8", "MultiMedia8 Capture", 0, 0, 0, 0),
 
+	SND_SOC_DAPM_AIF_IN("CS-VOICE_DL1", "CS-VOICE Playback", 0, 0, 0, 0),
+	SND_SOC_DAPM_AIF_OUT("CS-VOICE_UL1", "CS-VOICE Capture", 0, 0, 0, 0),
+
 	/* Mixer definitions */
 	SND_SOC_DAPM_MIXER("HDMI Mixer", SND_SOC_NOPM, 0, 0,
 			   hdmi_mixer_controls,
@@ -918,6 +921,13 @@ static const struct snd_soc_dapm_route intercon[] = {
 	{"MM_UL6", NULL, "MultiMedia6 Mixer"},
 	{"MM_UL7", NULL, "MultiMedia7 Mixer"},
 	{"MM_UL8", NULL, "MultiMedia8 Mixer"},
+
+	/* FIXME */
+	{"CS-VOICE_UL1", NULL, "TERT_MI2S_TX"},
+	{"PRI_MI2S_RX", NULL, "CS-VOICE_DL1"},
+
+	{"CS-VOICE_DL1", NULL, "CS-VOICE Playback" },
+	{"CS-VOICE Capture", NULL, "CS-VOICE_UL1"},
 };
 
 static int routing_hw_params(struct snd_soc_component *component,
