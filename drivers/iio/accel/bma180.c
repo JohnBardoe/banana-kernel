b5395f161b58900497a8ec1f234e188f5b494d53
Minecrell <minecrell@minecrell.net>
2021-05-25 16:36:31 +0200

    Revert "SUBMITME: iio: accel: bma180: BMA253 support"
    This reverts commit 10f7b0051b7896dfc22f8f7dc55cd1d8a5765fb7.

--- drivers/iio/accel/bma180.c
+++ drivers/iio/accel/bma180.c
@@ -1182,10 +1182,6 @@ static const struct of_device_id bma180_of_match[] = {
 		.data = (void *)BMA250
 	},
 	{
-		.compatible = "bosch,bma253",
-		.data = (void *)BMA254
-	},
-	{
 		.compatible = "bosch,bma254",
 		.data = (void *)BMA254
 	},
